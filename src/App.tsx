d01b114ef426290f2fb56ff4e2d6acac808357cf
Camelia Ciuca <camelia1414@gmail.com>
2024-08-27 12:31:38 +0200

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Counter from './componets/Counter'
+
+
+const App = () => {
+  return (
+    <div>
+      <h2>Redux practice</h2>
+      <Counter/>
+    </div>
+  )
+}
+
+export default App
